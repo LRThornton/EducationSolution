51317acf858d1a6ead56f8e7bc26a28fe4ac0423
leslie thornton <shodge51@aol.com>
2022-02-07 15:33:33 -0500

    Add project files.

---
+++ EducationProject/Major.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationProject {
+   
+    public class Major {
+        // this is static property
+        private static int NextId { get; set; } = 1;
+
+        public int Id { get; set; }
+        public string Description { get; set; }   
+        public int MinSAT { get; set; }
+
+        //this is the constructor
+        public Major(string description, int minSAT) {
+            Id = NextId++;  //or Id = NextId; NextId++;
+            Description = description;
+            MinSAT = minSAT;             
+
+        }
+      
+
+    }
+}
---
+++ EducationProject/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationProject {
+
+    public class Student {
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int SAT { get; set; }
+        public decimal GPA { get; set; }
+        public Major Major { get; set; } // the second "Major" could be fred (public major Fred)
+
+        //method
+        public Major GetMajorByDescription(string description, 
+                                        List<Major> majors) {
+            foreach(Major m in majors) {
+                if(description == m.Description) {
+                    return m;
+                }
+            }
+            return null;
+        }
+
+        //constructor
+
+        public Student(string firstname, string lastname, int sat, decimal gpa) {
+            Firstname = firstname;
+            Lastname = lastname;
+            SAT = sat;
+            GPA = gpa;
+            Major = null;
+
+        }
+
+
+    }
+}
